9b2d096675bd8d8ebd1a4d72c9bb9a617c383740
Wideka Ardhiguna <widekaardhiguna22@gmail.com>
2022-03-21 09:36:53 +0700

    simple counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react"
+
 function App() {
-  return <h1>Hello World !!</h1>
+  const [count, setCount] = useState(0)
+
+  const handleIncrease = () => {
+    setCount(count + 1)
+  }
+
+  const handleDecrease = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <div className="counter-wrapper">
+      <h2 className="indicator">Count: {count}</h2>
+      <button className="button decrease" onClick={handleDecrease}>
+        Decrease
+      </button>
+      <button className="button increase" onClick={handleIncrease}>
+        Increase
+      </button>
+    </div>
+  )
 }
 
 export default App
